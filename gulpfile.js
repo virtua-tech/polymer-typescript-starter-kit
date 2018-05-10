a0d06f35817ef93f230015cc870f7d9413bc7941
Sam Carecho <digiwise@gmail.com>
2018-05-10 10:26:53 +0700

    Solves issue #3

--- gulpfile.js
+++ gulpfile.js
@@ -10,12 +10,12 @@
 'use strict';
 const del = require('del');
 const gulp = require('gulp');
-const gulpif = require('gulp-if');
-const gulpIgnore = require('gulp-ignore');
+//const gulpif = require('gulp-if');
+//const gulpIgnore = require('gulp-ignore');
 const mergeStream = require('merge-stream');
 const polymerBuild = require('polymer-build');
 // Here we add tools that will be used to process our source files.
-const imagemin = require('gulp-imagemin');
+//const imagemin = require('gulp-imagemin');
 const plumber = require('gulp-plumber');
 const sourcemaps = require('gulp-sourcemaps');
 const ts = require('gulp-typescript');
@@ -96,7 +96,7 @@ function build() {
         const sourcesStreamSplitter = new polymerBuild.HtmlSplitter();
         const dependenciesStreamSplitter = new polymerBuild.HtmlSplitter();
         // Okay, so first thing we do is clear the build directory
-        console.log("Deleting " + buildDirectory + " directory...");
+        console.log('Deleting ' + buildDirectory + ' directory...');
         del([buildDirectory])
             .then(function() {
                 // Let's start by getting your source files. These are all the files
